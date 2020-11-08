1e5dbb545a23b7fcd1469fe90bcf7f3ca603e622
danimal141 <hideaki.ishii1204@gmail.com>
2020-11-08 16:05:55 +0900

    Add sample graphql server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,20 @@
-console.log('---Hello')
+import { ApolloServer } from 'apollo-server'
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+`
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => 40
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen().then(({ url }) => console.log(`Service running on ${url}`))
