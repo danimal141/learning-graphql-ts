d12698ee0649254050cbe0792118f1719830d14d
danimal141 <hideaki.ishii1204@gmail.com>
2020-11-08 16:19:51 +0900

    Add mutation example

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,11 +4,30 @@ const typeDefs = `
   type Query {
     totalPhotos: Int!
   }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Boolean
+  }
 `
+
+type Photo = {
+  name: string,
+  description: string | null
+}
+
+let photos: Photo[] = []
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 40
+    totalPhotos: () => photos.length
+  },
+
+  Mutation: {
+    postPhoto(_: any, photo: Photo) {
+      photos.push(photo)
+      console.log(`------Photos: ${photos}`)
+      return true
+    }
   }
 }
 
@@ -16,5 +35,4 @@ const server = new ApolloServer({
   typeDefs,
   resolvers
 })
-
 server.listen().then(({ url }) => console.log(`Service running on ${url}`))
