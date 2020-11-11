e501a76e55d6bf1bd5227c055c7f44e193e95fa7
danimal141 <hideaki.ishii1204@gmail.com>
2020-11-11 23:30:18 +0900

    Prepare base routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
 import fs from 'fs'
 import path from 'path'
 import resolvers from './resolvers'
 
 const typeDefs = fs.readFileSync(path.join(__dirname, '../../graphql/schema.graphql')).toString()
-
+const app = express()
 const server = new ApolloServer({
   typeDefs,
   resolvers
 })
 
-server.listen().then(({ url }) => console.log(`Service running on ${url}`))
+app.get('/', (_req, res) => res.end('Welcome!'))
+app.listen({ port: 4000 }, () => console.log(`Server running @ localhost:4000${server.graphqlPath}`))
