25c89fba51ade7144fe7cc7e3ffd358053822a83
danimal141 <hideaki.ishii1204@gmail.com>
2020-12-29 14:53:27 +0900

    Add user queries

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Int } from "type-graphql";
 import { AuthContext } from "../index";
 import User from "../entities/User";
 
@@ -7,5 +7,16 @@ export default class UserResolver {
   @Query((_returns) => User, { nullable: true })
   async me(@Ctx() ctx: AuthContext): Promise<User | null> {
     return ctx.currentUser;
+  }
+
+  @Query((_returns) => Int)
+  async totalUsers(): Promise<number> {
+    return await User.count();
+  }
+
+  // It may create a crazy query
+  @Query((_returns) => [User])
+  async allUsers(): Promise<User[]> {
+    return await User.find();
   }
 }
