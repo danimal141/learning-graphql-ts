3462f705f78f8b6ffdd0984313a9db966a52dc48
danimal141 <hideaki.ishii1204@gmail.com>
2020-12-30 22:54:54 +0900

    Update vulnerable libraries

--- client/src/graphql/generated.tsx
+++ client/src/graphql/generated.tsx
@@ -1,13 +1,9 @@
-import { gql } from "@apollo/client";
-import * as Apollo from "@apollo/client";
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -20,113 +16,126 @@ export type Scalars = {
 };
 
 export type AuthPayload = {
-  __typename?: "AuthPayload";
-  token: Scalars["String"];
+  __typename?: 'AuthPayload';
+  token: Scalars['String'];
   user: User;
 };
 
+
 export type Mutation = {
-  __typename?: "Mutation";
+  __typename?: 'Mutation';
   addFakeUsers: Array<User>;
   createPhoto: Photo;
   githubAuth: AuthPayload;
 };
 
+
 export type MutationAddFakeUsersArgs = {
-  count: Scalars["Float"];
+  count: Scalars['Float'];
 };
+
 
 export type MutationCreatePhotoArgs = {
   photo: PostPhotoInput;
 };
 
+
 export type MutationGithubAuthArgs = {
-  code: Scalars["String"];
+  code: Scalars['String'];
 };
 
 export type Photo = {
-  __typename?: "Photo";
+  __typename?: 'Photo';
   category: PhotoCategory;
-  createdAt: Scalars["DateTime"];
-  description?: Maybe<Scalars["String"]>;
-  id: Scalars["ID"];
-  name: Scalars["String"];
+  createdAt: Scalars['DateTime'];
+  description?: Maybe<Scalars['String']>;
+  id: Scalars['ID'];
+  name: Scalars['String'];
   postedBy: User;
   taggedUsers: Array<User>;
-  url: Scalars["String"];
+  url: Scalars['String'];
 };
 
 export enum PhotoCategory {
-  Action = "Action",
-  Graphic = "Graphic",
-  Landscape = "Landscape",
-  Portrait = "Portrait",
-  Selfie = "Selfie",
+  Action = 'Action',
+  Graphic = 'Graphic',
+  Landscape = 'Landscape',
+  Portrait = 'Portrait',
+  Selfie = 'Selfie'
 }
 
 export type PostPhotoInput = {
-  category?: Maybe<Scalars["String"]>;
-  description?: Maybe<Scalars["String"]>;
-  name: Scalars["String"];
+  category?: Maybe<Scalars['String']>;
+  description?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
 };
 
 export type Query = {
-  __typename?: "Query";
+  __typename?: 'Query';
   allPhotos: Array<Photo>;
   allUsers: Array<User>;
   me?: Maybe<User>;
-  totalPhotos: Scalars["Int"];
-  totalUsers: Scalars["Int"];
+  totalPhotos: Scalars['Int'];
+  totalUsers: Scalars['Int'];
 };
 
 export type User = {
-  __typename?: "User";
-  avatar: Scalars["String"];
-  createdAt: Scalars["DateTime"];
-  githubLogin: Scalars["ID"];
+  __typename?: 'User';
+  avatar: Scalars['String'];
+  createdAt: Scalars['DateTime'];
+  githubLogin: Scalars['ID'];
   inPhotos: Array<Photo>;
-  name: Scalars["String"];
+  name: Scalars['String'];
   postedPhotos: Array<Photo>;
 };
 
 export type LoginMutationVariables = Exact<{
-  code: Scalars["String"];
+  code: Scalars['String'];
 }>;
 
-export type LoginMutation = { __typename?: "Mutation" } & {
-  githubAuth: { __typename?: "AuthPayload" } & Pick<AuthPayload, "token">;
-};
 
-export type UserInfoFragment = { __typename?: "User" } & Pick<
-  User,
-  "githubLogin" | "name" | "avatar"
->;
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & { githubAuth: (
+    { __typename?: 'AuthPayload' }
+    & Pick<AuthPayload, 'token'>
+  ) }
+);
 
-export type UsersQueryVariables = Exact<{ [key: string]: never }>;
+export type UserInfoFragment = (
+  { __typename?: 'User' }
+  & Pick<User, 'githubLogin' | 'name' | 'avatar'>
+);
 
-export type UsersQuery = { __typename?: "Query" } & {
-  allUsers: Array<{ __typename?: "User" } & UserInfoFragment>;
-  me?: Maybe<{ __typename?: "User" } & UserInfoFragment>;
-};
+export type UsersQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type UsersQuery = (
+  { __typename?: 'Query' }
+  & { allUsers: Array<(
+    { __typename?: 'User' }
+    & UserInfoFragment
+  )>, me?: Maybe<(
+    { __typename?: 'User' }
+    & UserInfoFragment
+  )> }
+);
 
 export const UserInfoFragmentDoc = gql`
-  fragment UserInfo on User {
-    githubLogin
-    name
-    avatar
-  }
-`;
+    fragment UserInfo on User {
+  githubLogin
+  name
+  avatar
+}
+    `;
 export const LoginDocument = gql`
-  mutation Login($code: String!) {
-    githubAuth(code: $code) {
-      token
-    }
+    mutation Login($code: String!) {
+  githubAuth(code: $code) {
+    token
   }
-`;
-export type LoginMutationFn = Apollo.MutationFunction<
-  LoginMutation,
-  LoginMutationVariables
->;
+}
+    `;
+export type LoginMutationFn = Apollo.MutationFunction<LoginMutation, LoginMutationVariables>;
 
 /**
  * __useLoginMutation__
@@ -145,34 +154,22 @@ export type LoginMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useLoginMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    LoginMutation,
-    LoginMutationVariables
-  >
-) {
-  return Apollo.useMutation<LoginMutation, LoginMutationVariables>(
-    LoginDocument,
-    baseOptions
-  );
-}
+export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
+        return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, baseOptions);
+      }
 export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
 export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
-export type LoginMutationOptions = Apollo.BaseMutationOptions<
-  LoginMutation,
-  LoginMutationVariables
->;
+export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
 export const UsersDocument = gql`
-  query Users {
-    allUsers {
-      ...UserInfo
-    }
-    me {
-      ...UserInfo
-    }
+    query Users {
+  allUsers {
+    ...UserInfo
   }
-  ${UserInfoFragmentDoc}
-`;
+  me {
+    ...UserInfo
+  }
+}
+    ${UserInfoFragmentDoc}`;
 
 /**
  * __useUsersQuery__
@@ -189,25 +186,12 @@ export const UsersDocument = gql`
  *   },
  * });
  */
-export function useUsersQuery(
-  baseOptions?: Apollo.QueryHookOptions<UsersQuery, UsersQueryVariables>
-) {
-  return Apollo.useQuery<UsersQuery, UsersQueryVariables>(
-    UsersDocument,
-    baseOptions
-  );
-}
-export function useUsersLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<UsersQuery, UsersQueryVariables>
-) {
-  return Apollo.useLazyQuery<UsersQuery, UsersQueryVariables>(
-    UsersDocument,
-    baseOptions
-  );
-}
+export function useUsersQuery(baseOptions?: Apollo.QueryHookOptions<UsersQuery, UsersQueryVariables>) {
+        return Apollo.useQuery<UsersQuery, UsersQueryVariables>(UsersDocument, baseOptions);
+      }
+export function useUsersLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<UsersQuery, UsersQueryVariables>) {
+          return Apollo.useLazyQuery<UsersQuery, UsersQueryVariables>(UsersDocument, baseOptions);
+        }
 export type UsersQueryHookResult = ReturnType<typeof useUsersQuery>;
 export type UsersLazyQueryHookResult = ReturnType<typeof useUsersLazyQuery>;
-export type UsersQueryResult = Apollo.QueryResult<
-  UsersQuery,
-  UsersQueryVariables
->;
+export type UsersQueryResult = Apollo.QueryResult<UsersQuery, UsersQueryVariables>;
